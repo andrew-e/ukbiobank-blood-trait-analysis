a7f764fed3b4526bee32b69c46d793c44d85a70c
Andrew Elmore <andrew.r.elmore@gmail.com>
2022-07-01 21:28:18 +0100

    Bayesian linear regression in working order

--- ml/bayesian_regression.sh
+++ ml/bayesian_regression.sh
@@ -1,8 +1,17 @@
-#PBS -l select=1:ncpus=1:mem=256gb
+#PBS -l select=1:ncpus=4:mem=512gb
 #PBS -l walltime=4:0:0
-
-echo "Running job for $MARKER and $ETHNICITY"
-SNP_DIR=CHR_1_SNP_rs174574
+#PBS -J 0-631
 
 module load anaconda3/personal
-Rscript /rds/general/user/are20/home/ukbiobank-blood-trait-analysis/ml/bayesian_regression_for_snp.R $MARKER $ETHNICITY
+source activate r4
+
+cd /rds/general/user/are20/ephemeral/ml/pct/
+SNPS=($(ls))
+SNP=${SNPS[$PBS_ARRAY_INDEX]}
+SNP=${SNP:4}
+cd -
+
+echo "Running job for $MARKER and $ETHNICITY and $SNP"
+
+Rscript /rds/general/user/are20/home/ukbiobank-blood-trait-analysis/ml/bayesian_regression_for_snp.R $MARKER $ETHNICITY $SNP
+
--- ml/bayesian_regression_for_snp.R
+++ ml/bayesian_regression_for_snp.R
@@ -1,5 +1,4 @@
 library(data.table)
-require(dplyr)
 library(rstanarm)
 options(mc.cores = parallel::detectCores())
 #You can read up on bayesian linear regression and stan_glm here:https://cran.r-project.org/web/packages/rstanarm/rstanarm.pdf#page.108
@@ -11,46 +10,99 @@ ethnicity <- args[2]
 snp <- args[3]
 
 EPHEMERAL_DIR <- paste0("/rds/general/user/are20/ephemeral/ml/", marker, "/SNP_", snp)
+SCORE_FILENAME <- sprintf("results/ml/scores/%s_%s.score", marker, ethnicity)
+
+#skip if already calculated...
+if (!file.exists(SCORE_FILENAME)) {
+  file.create(SCORE_FILENAME)
+}
+score_file <- fread(SCORE_FILENAME)
+if (any(score_file$V4 == snp)) {
+  print(paste(snp, "already calculated and stored in score file, skipping."))
+  q()
+}
+
 residuals <- fread(sprintf("results/residuals/%s_%s_residuals.csv", ethnicity, marker), sep=',', header=TRUE, stringsAsFactors=FALSE)
+
+calculateR2 <- function(stan_glm) {
+  if (is.null(stan_glm)) {
+    return(NA)
+  }
+  
+  ss_res <- var(residuals(stan_glm))
+  ss_total <- var(fitted(stan_glm)) + var(residuals(stan_glm))
+  r2 <- 1 - (ss_res / ss_total)
+  return(r2)
+}
+
+calculateBayesianGLM <- function(surrounding_snp, includePCs) {
+  snp_no_trail <- gsub('.{2}$', '', surrounding_snp)
+  ss_snp <- summary_statistic[summary_statistic$ID == snp_no_trail]
+  
+  plink_subset <- subset(plink_data_ethnicity, select = c("id", "eid_19266", "residuals", surrounding_snp))
+  scale <- ss_snp$SE * sqrt(410000) #410000 was the sample size of the white pop
+  location <- ss_snp$EFFECT
+  
+  glm_formula <- paste0("residuals ~ ", paste0("`", surrounding_snp, "`"))
+  if (includePCs == T) {
+    plink_subset <- merge(plink_subset, principcal_components, by.x="id", by.y="eid")
+    pc_names <- colnames(plink_subset)[-4:-1]
+    glm_formula = paste0("residuals ~ ", paste0("`", surrounding_snp, "`"), " + ", paste(pc_names, collapse=" + "))
+  }
+  
+  if(sd(plink_subset[[surrounding_snp]], na.rm=TRUE) == 0) {
+    print(paste("No variation in", snp_no_trail, "skipping."))
+    return()
+  }
+  print(paste("Calculating for ", snp_no_trail, location, scale))
+  
+  stan_model <- NULL
+  if (nrow(ss_snp) == 1) {
+    stan_model <- stan_glm(as.formula(glm_formula), data = plink_subset, prior = normal(location = location, scale = scale, autoscale = FALSE), refresh = 0)
+  }
+  else {
+    stan_model <- stan_glm(as.formula(glm_formula), data = plink_subset, refresh = 0)
+  }
+  return(stan_model)
+}
+
+
+plink_data <- fread(paste0(EPHEMERAL_DIR, "/plink.raw"))
+plink_data = as.data.frame.matrix(plink_data)
+plink_data <- plink_data[, !duplicated(colnames(plink_data))]
+plink_data <- as.data.frame(plink_data)
+
+snp_list <- colnames(plink_data)[-6:-1]
+plink_data_ethnicity <- merge(residuals, plink_data, by.x="eid_19266", by.y="FID")
 
 #Get summary statistics, to be used for priors in the bayesian linear regression.
 summary_stat_columns <- c("ID", "CHR", "EFFECT", "SE")
 summary_statistic <- fread(sprintf("data/plink/summary_statistics/%s.assoc", marker), select = summary_stat_columns)
-ss_snp <- summary_statistic[summary_statistic$ID == snp]
 
 #get PCs and plink raw output
 principcal_components <- fread("data/plink/UKB_app_69328_genetic_PCs.txt")
 
-plink_data <- fread(paste0(EPHEMERAL_DIR, "/plink.raw"))
-plink_data <- plink_data %>% select(unique(colnames(.))) #removing duplicate columns, since multiple rows of the same SNP are found in the .bim files
-plink_data_ethnicity <- merge(residuals, plink_data, by.x="eid_19266", by.y="FID")
+stan_glm_pcs <- lapply(snp_list, calculateBayesianGLM, includePCs=T)
 
-snp_list <- colnames(plink_data)[-6:-1]
-for (surrounding_snp in snp_list) {
-  plink_subset <- subset(plink_data_ethnicity, select = c("id", "eid_19266", "residuals", surrounding_snp))
-  snp_with_pcs <- merge(plink_subset, principcal_components, by.x="id", by.y="eid")
-  names(snp_with_pcs)[4] <- "snp_dosage"
+r2_pcs <- lapply(stan_glm_pcs, calculateR2)
+stan_comparison <- data.frame(snp = unlist(snp_list), r2_pcs = unlist(r2_pcs))
+best_performing_snp <- stan_comparison[which.max(stan_comparison$r2_pcs),]
 
-  #I think I have the relevant data..
-  #The coded SNP for all the relevant ethnic background, annotated with the residuals of the blood marker
+print("Best performing SNP with r2 (incl pcs): ")
+print(best_performing_snp)
 
-  sample_size <- nrow(snp_with_pcs)
-  scale <- ss_snp$SE * sqrt(447409)
-  dosage_breakdown <- table(snp_with_pcs$snp_dosage) #maybe this needs to be the snp we start with, not calculated for each snp...
-  location <- as.numeric((ss_snp$EFFECT * dosage_breakdown[2] / sample_size) + (ss_snp$EFFECT * 2 * dosage_breakdown[3] / sample_size))
-  
-  #run stan model with and without PCs I guess?
-  stan_model_with_prior <- stan_glm(residuals ~ snp_dosage, data = snp_with_pcs, prior = normal(location = location, scale = scale))
-  
-  pc_names <- colnames(snp_with_pcs)[-4:-1]
-  formula_with_pcs = paste0("residuals ~ snp_dosage + ", paste(pc_names, collapse=" + "))
-  stan_model_with_prior_and_pcs <- stan_glm(as.formula(formula_with_pcs), data = snp_with_pcs, prior = normal(location = location, scale = scale))
-  
-  prior_summary(stan_model_with_prior)
-  prior_summary(stan_model_with_prior_and_pcs)
-  
-  summary(stan_model_with_prior)
-  summary(stan_model_with_prior_and_pcs)
-  
-  #do something with the results and save them to a table?!?
+best_snp_no_trail <- gsub('.{2}$', '', best_performing_snp$snp)
+ss_snp <- summary_statistic[summary_statistic$ID == best_snp_no_trail]
+
+
+if (nrow(ss_snp) == 1) {
+  bim <- fread(sprintf("/rds/general/apps/RDS_COMPATIBILITY_LINKFARM/groupvol/med-bio/uk-biobank-2017/release_12032018/converted_data/imp_bgen1.1_plink/ukb_imp_chr%s_v3.bim", ss_snp$CHR))
+  minor_allele <- bim[bim$V2 == ss_snp$ID]$V5
+  effect <- ss_snp$EFFECT
+} else {
+  minor_allele <- NA
+  effect <- NA
 }
+
+score_file_entry <- paste(best_snp_no_trail, minor_allele, effect, snp)
+write(score_file_entry, file = SCORE_FILENAME, append = TRUE)
