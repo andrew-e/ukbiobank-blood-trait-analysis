4296dd9184a8199defbba6a59d29c4fcb69aac71
Andrew Elmore <andrew.r.elmore@gmail.com>
2022-07-10 20:05:31 +0100

    Simplifying bayesian code by removing unnecessary stuff

--- ml/bayesian_regression_for_snp.R
+++ ml/bayesian_regression_for_snp.R
@@ -12,6 +12,16 @@ snp <- args[3]
 EPHEMERAL_DIR <- paste0("/rds/general/user/are20/ephemeral/ml/", marker, "/SNP_", snp)
 SCORE_FILENAME <- sprintf("results/ml/scores/%s_%s.score", marker, ethnicity)
 
+#Get summary statistics, to be used for priors in the bayesian linear regression.
+summary_stat_columns <- c("ID", "CHR", "EFFECT", "SE")
+summary_statistic <- fread(sprintf("data/plink/summary_statistics/%s.assoc", marker), select = summary_stat_columns)
+principcal_components <- fread("data/plink/UKB_app_69328_genetic_PCs.txt")
+
+plink_data <- fread(paste0(EPHEMERAL_DIR, "/plink.raw"))
+plink_data = as.data.frame.matrix(plink_data)
+plink_data <- plink_data[, !duplicated(colnames(plink_data))]
+plink_data <- as.data.frame(plink_data)
+
 #skip if already calculated...
 if (!file.exists(SCORE_FILENAME)) {
   file.create(SCORE_FILENAME)
@@ -41,20 +51,18 @@ calculateBayesianGLM <- function(surrounding_snp, includePCs) {
   ss_snp <- summary_statistic[summary_statistic$ID == snp_no_trail]
   
   plink_subset <- subset(plink_data_ethnicity, select = c("id", "eid_19266", "residuals", surrounding_snp))
-  scale <- ss_snp$SE * sqrt(410000) #410000 was the sample size of the white pop
-  location <- ss_snp$EFFECT
-  
-  glm_formula <- paste0("residuals ~ ", paste0("`", surrounding_snp, "`"))
-  if (includePCs == T) {
-    plink_subset <- merge(plink_subset, principcal_components, by.x="id", by.y="eid")
-    pc_names <- colnames(plink_subset)[-4:-1]
-    glm_formula = paste0("residuals ~ ", paste0("`", surrounding_snp, "`"), " + ", paste(pc_names, collapse=" + "))
-  }
+
+  plink_subset <- merge(plink_subset, principcal_components, by.x="id", by.y="eid")
+  pc_names <- colnames(plink_subset)[-4:-1]
+  glm_formula = paste0("residuals ~ ", paste0("`", surrounding_snp, "`"), " + ", paste(pc_names, collapse=" + "))
   
   if(sd(plink_subset[[surrounding_snp]], na.rm=TRUE) == 0) {
     print(paste("No variation in", snp_no_trail, "skipping."))
     return()
   }
+  
+  scale <- ss_snp$SE * sqrt(410000) #410000 was the sample size of the white pop
+  location <- ss_snp$EFFECT
   print(paste("Calculating for ", snp_no_trail, location, scale))
   
   stan_model <- NULL
@@ -68,12 +76,6 @@ calculateBayesianGLM <- function(surrounding_snp, includePCs) {
   return(stan_model)
 }
 
-
-plink_data <- fread(paste0(EPHEMERAL_DIR, "/plink.raw"))
-plink_data = as.data.frame.matrix(plink_data)
-plink_data <- plink_data[, !duplicated(colnames(plink_data))]
-plink_data <- as.data.frame(plink_data)
-
 snp_list <- colnames(plink_data)[-6:-1]
 plink_data_ethnicity <- merge(residuals, plink_data, by.x="eid_19266", by.y="FID")
 
@@ -81,12 +83,6 @@ if (ethnicity == "white") {
   plink_data_ethnicity <- plink_data_ethnicity[1:50000,]
 }
 
-#Get summary statistics, to be used for priors in the bayesian linear regression.
-summary_stat_columns <- c("ID", "CHR", "EFFECT", "SE")
-summary_statistic <- fread(sprintf("data/plink/summary_statistics/%s.assoc", marker), select = summary_stat_columns)
-
-#get PCs and plink raw output
-principcal_components <- fread("data/plink/UKB_app_69328_genetic_PCs.txt")
 
 stan_glm_pcs <- lapply(snp_list, calculateBayesianGLM, includePCs=T)
 
@@ -100,15 +96,9 @@ print(best_performing_snp)
 best_snp_no_trail <- gsub('.{2}$', '', best_performing_snp$snp)
 ss_snp <- summary_statistic[summary_statistic$ID == best_snp_no_trail]
 
-
-if (nrow(ss_snp) == 1) {
-  bim <- fread(sprintf("/rds/general/apps/RDS_COMPATIBILITY_LINKFARM/groupvol/med-bio/uk-biobank-2017/release_12032018/converted_data/imp_bgen1.1_plink/ukb_imp_chr%s_v3.bim", ss_snp$CHR))
-  minor_allele <- bim[bim$V2 == ss_snp$ID]$V5
-  effect <- ss_snp$EFFECT
-} else {
-  minor_allele <- NA
-  effect <- NA
-}
+bim <- fread(sprintf("/rds/general/apps/RDS_COMPATIBILITY_LINKFARM/groupvol/med-bio/uk-biobank-2017/release_12032018/converted_data/imp_bgen1.1_plink/ukb_imp_chr%s_v3.bim", ss_snp$CHR))
+minor_allele <- bim[bim$V2 == ss_snp$ID]$V5
+effect <- ss_snp$EFFECT
 
 score_file_entry <- paste(best_snp_no_trail, minor_allele, effect, snp)
 write(score_file_entry, file = SCORE_FILENAME, append = TRUE)
